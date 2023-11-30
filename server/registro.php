96cae07c878d7f8d8c4140b45afec440337ab17d
Bowie <carolinyrochajobs@gmail.com>
2023-11-30 20:06:07 -0300

    creation of the registro.php page and some changes

---
+++ server/registro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/CabecalhoRodape.css">
+    <link rel="stylesheet" href="style/loginS.css">
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <?php
+        include_once 'components/nav.php';
+    ?>
+    <header class="text-center">
+        <div class="titulo d-flex align-items-center justify-content-center flex-column">
+            <h3>Crie sua conta!</h3>
+            <img src="images/Rectangle 1.png">
+        </div>
+    </header>
+    <main class="flex-grow-1 d-flex justify-content-center align-items-center">
+    <form action="" method="post" class="d-flex flex-column my-4 formu">
+        <input type="text" name="nome" class="form-control" placeholder="Nome" required>
+        <input type="text" name="email" class="form-control" placeholder="Email" required>
+        <input type="text" name="telefone" class="form-control" placeholder="Telefone" required>
+        <input type="text" name="cep" class="form-control" placeholder="CEP" required>
+        <input type="text" name="rua" class="form-control" placeholder="Rua" required>
+        <input type="text" name="bairro" class="form-control" placeholder="Bairro" required>
+        <input type="text" name="numero" class="form-control" placeholder="Número do Endereço" required>
+        <input type="password" name="senha" class="form-control" placeholder="Senha" required>
+        <input type="submit" value="Registre-se" class="btn btn-primary mt-3">
+    </form>
+</main>
+
+    <?php
+      /* include_once 'components/footer.php';*/
+    ?>
+</body>
+</html>
