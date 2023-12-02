91d5950beb6c78924c2b14a33192c936b1a1df11
gabrieibarboza <gabrielklarkgk@gmail.com>
2023-12-02 09:03:50 -0300

    ✨ add more to dabatase

--- database/db_sorveteria.sql
+++ database/db_sorveteria.sql
@@ -10,6 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE SCHEMA IF NOT EXISTS `db_sorveteria` DEFAULT CHARACTER SET utf8 ;
 USE `db_sorveteria` ;
 
+
 -- -----------------------------------------------------
 -- Table `db_sorveteria`.`tbEndereco`
 -- -----------------------------------------------------
