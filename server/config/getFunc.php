9bf05513e439bf78c5af56d89197fae536d32514
Jéssica <134964436+Jessybr@users.noreply.github.com>
2023-12-06 11:34:27 -0300

    ✨ Update file
    Foi adicionado na parte da tag de "img": 
    "images/" e ".png".

--- server/config/getFunc.php
+++ server/config/getFunc.php
@@ -18,23 +18,23 @@ do {
         <div class="c2">
             <div class="c3">
                 <picture>
-                <source media="(min-width: 768px)" srcset="'.$row["profile"].'">
-                <img src="'.$row["profile"].'" alt="'.$row["nomeFuncionario"].'">
+                <source media="(min-width: 768px)" srcset="images/'.$row["profile"].'.png">
+                <img src="images/'.$row["profile"].'.png" alt="'.$row["nomeFuncionario"].'">
                 </picture>
             </div>
             <div class="d-flex flex-column">
                 <div id="dados">
                     <h3 class="titulo px-3">'.$row["nomeFuncionario"].'</h3>
                     <div class="px-3">
-                        <p>Email:'.$row["emailFuncionario"].'</p>
-                        <p>Celular:'.$row["telefoneFuncionario"].'</p>
+                        <p>Email: '.$row["emailFuncionario"].'</p>
+                        <p>Celular: '.$row["telefoneFuncionario"].'</p>
                     </div>
                 </div>
             </div>        
         </div>
         <div class="botao text-center d-flex justify-content-evenly mt-3">
-            <button><a href="'.$redirectToEditar.'">Editar</a></button>        
-            <button><a href="'.$redirectToExcluir.'">Excluir</a></button>        
+            <button id="excl"><a href="'.$redirectToEditar.'">Editar</a></button>        
+            <button id="edit"><a href="'.$redirectToExcluir.'">Excluir</a></button>        
         </div>
         </div>';
     }
