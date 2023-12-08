61bd53056cebefe7d3b2b0879ae2d229dae2486c
Gabriel Barboza <65047738+gabrieIbarboza@users.noreply.github.com>
2023-12-07 21:24:19 -0300

    Revert "Laura func"

--- server/script/endereco.js
+++
@@ -1,7 +0,0 @@
-const btnAddEnd=document.querySelector("#btnAddEnd");
-const formuEnd=document.querySelector("#formuAddEnd");
-
-btnAddEnd.addEventListener("click", ()=>{
-    formuEnd.classList.add("exibir");
-    btnAddEnd.style.display="none";
-}); 
